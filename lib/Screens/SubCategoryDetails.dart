f0294efa8f91a52ed3e7db89404b9ad9df66e525
developerkrishnasoftweb <developer.krishnasoftweb@gmail.com>
2021-02-19 10:06:16 +0530

    19/02 Morning

--- lib/Screens/SubCategoryDetails.dart
+++ lib/Screens/SubCategoryDetails.dart
@@ -7,6 +7,7 @@ class SubcategoryDetails extends StatefulWidget {
 }
 
 class _SubcategoryDetailsState extends State<SubcategoryDetails> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   List<Category> categories = [
     Category(
         title: "Atta, Flours & Sooji",
@@ -26,10 +27,69 @@ class _SubcategoryDetailsState extends State<SubcategoryDetails> {
         title: "Masalas Spices",
         image: "https://www.jiomart.com/images/category/15/thumb/0-15.png"),
   ];
+  List<String> products = [
+    "ALL",
+    "Exotic Vegetables",
+    "Citrus Fruits",
+    "Root Vegetable",
+    "Beans",
+    "Guard",
+    "Vegetable Others"
+  ];
+  List<Item> items = [
+    Item(
+        image:
+            "https://www.jiomart.com/images/product/75x75/590122516/fortune-health-physically-refined-rice-bran-oil-5-l-india-gate-feast-rozzana-basmati-rice-5-kg-combo-pack-0-20201229.jpg",
+        category: "Groccery",
+        price: 12,
+        name: "Aashirvaad Multigrain Atta 5 kg",
+        quantity: 1,
+        discount: 10,
+        maxQuantity: 5),
+    Item(
+        image:
+            "https://www.jiomart.com/images/product/75x75/491504124/super-sarvottam-physicaly-refined-100-rice-bran-oil-1-l-pouch-0-20201021.jpg",
+        category: "Groccery",
+        price: 200,
+        name: "Tata Lite Free Flow Iodised Salt 1 kg",
+        quantity: 1,
+        discount: 30,
+        maxQuantity: 1),
+    Item(
+        image:
+            "https://www.jiomart.com/images/product/75x75/491076026/oleev-active-ricebran-based-blended-oil-5-l-jar-0-20200825.jpg",
+        category: "Groccery",
+        price: 121.0,
+        name: "Good Life Tur Dal 1 kg",
+        quantity: 5,
+        discount: 20,
+        inCart: true,
+        maxQuantity: 20),
+    Item(
+        image:
+            "https://www.jiomart.com/images/product/75x75/491278308/priya-fortified-with-vitamin-a-d-refined-sunflower-oil-1-l-0-20210119.jpg",
+        category: "Groccery",
+        price: 123,
+        name: "Good Life Almonds 500 g",
+        quantity: 1,
+        discount: 15,
+        maxQuantity: 10),
+    Item(
+        image:
+            "https://www.jiomart.com/images/product/75x75/491076025/oleev-active-ricebran-based-blended-oil-1-l-pouch-0-20200805.jpg",
+        category: "Groccery",
+        price: 200,
+        name: "Tata Lite Free Flow Iodised Salt 1 kg",
+        quantity: 1,
+        discount: 30,
+        maxQuantity: 1),
+  ];
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: Text("Product",
             style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
@@ -112,40 +172,39 @@ class _SubcategoryDetailsState extends State<SubcategoryDetails> {
                                       ),
                                       trailing: IconButton(
                                         icon: Icon(Icons.close),
-                                        onPressed: () =>
-                                            Navigator.pop(context),
+                                        onPressed: () => Navigator.pop(context),
                                         splashRadius: 25,
                                       ),
                                     ),
                                     Expanded(
                                         child: ListView.separated(
-                                          separatorBuilder: (_, index) {
-                                            return Divider(
-                                              color: Colors.grey,
-                                            );
-                                          },
-                                          itemBuilder: (_, index) {
-                                            return ListTile(
-                                              leading: Image.network(
-                                                "https://www.jiomart.com/images/category/141/thumb/fruits-vegetables-20200520.png",
-                                                width: 40,
-                                                height: 40,
-                                              ),
-                                              title: Text(
-                                                "Fruits & Vegetables",
-                                                style: TextStyle(
-                                                    fontWeight: FontWeight.bold),
-                                              ),
-                                              subtitle: Text(
-                                                "Fresh Fruits,Fresh Vegetables,Herbs & Season ,Exotic Fruits & Vegetables",
-                                                maxLines: 1,
-                                                overflow: TextOverflow.ellipsis,
-                                              ),
-                                              onTap: () {},
-                                            );
-                                          },
-                                          itemCount: 10,
-                                        ))
+                                      separatorBuilder: (_, index) {
+                                        return Divider(
+                                          color: Colors.grey,
+                                        );
+                                      },
+                                      itemBuilder: (_, index) {
+                                        return ListTile(
+                                          leading: Image.network(
+                                            "https://www.jiomart.com/images/category/141/thumb/fruits-vegetables-20200520.png",
+                                            width: 40,
+                                            height: 40,
+                                          ),
+                                          title: Text(
+                                            "Fruits & Vegetables",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          subtitle: Text(
+                                            "Fresh Fruits,Fresh Vegetables,Herbs & Season ,Exotic Fruits & Vegetables",
+                                            maxLines: 1,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          onTap: () {},
+                                        );
+                                      },
+                                      itemCount: 10,
+                                    ))
                                   ],
                                 );
                               },
@@ -154,6 +213,7 @@ class _SubcategoryDetailsState extends State<SubcategoryDetails> {
                             );
                           });
                     },
+                    color: Colors.grey[100],
                   ),
                 ),
                 Expanded(
@@ -172,7 +232,7 @@ class _SubcategoryDetailsState extends State<SubcategoryDetails> {
                           },
                           child: Container(
                             margin:
-                            EdgeInsets.only(left: 10, bottom: 7, top: 7),
+                                EdgeInsets.only(left: 10, bottom: 7, top: 7),
                             padding: EdgeInsets.symmetric(horizontal: 5),
                             width: 130,
                             height: 76,
@@ -194,14 +254,14 @@ class _SubcategoryDetailsState extends State<SubcategoryDetails> {
                                 SizedBox(width: 10),
                                 Expanded(
                                     child: Text(
-                                      categories[index].title,
-                                      maxLines: 3,
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: categories[index].isSelected
-                                              ? Myapp.primaryColor
-                                              : Colors.black),
-                                    ))
+                                  categories[index].title,
+                                  maxLines: 3,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: categories[index].isSelected
+                                          ? Myapp.primaryColor
+                                          : Colors.black),
+                                ))
                               ],
                             ),
                           ));
@@ -239,21 +299,180 @@ class _SubcategoryDetailsState extends State<SubcategoryDetails> {
             height: 50,
             width: size.width,
             padding: EdgeInsets.symmetric(vertical: 5),
-            child: ListView.separated(
-              separatorBuilder: (_, index) {
-                return SizedBox(width: 10);
-              },
+            child: ListView.builder(
               itemBuilder: (_, index) {
-                return FlatButton(onPressed: () {}, child: Text("Carrot"), color: Colors.grey[300],);
+                return Container(
+                  margin: EdgeInsets.only(left: index == 0 ? 10 : 0, right: 10),
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text(products[index]),
+                    color: Colors.grey[300],
+                  ),
+                );
               },
               physics: BouncingScrollPhysics(),
-              itemCount: 10,
+              itemCount: products.length,
               scrollDirection: Axis.horizontal,
             ),
           ),
-          Expanded(child: ListView.builder(itemBuilder: (_, index) {
-            return Container();
-          }, itemCount: 10,))
+          Expanded(
+              child: ListView.separated(
+            separatorBuilder: (_, index) {
+              return Divider(
+                color: Colors.grey,
+                indent: 20,
+                endIndent: 20,
+              );
+            },
+            itemBuilder: (_, index) {
+              return card(items[index]);
+            },
+            itemCount: items.length,
+          ))
+        ],
+      ),
+    );
+  }
+
+  Widget card(Item item) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(
+            item.image,
+            width: 100,
+            height: 100,
+            fit: BoxFit.contain,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.name,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 10),
+                  RichText(
+                    text: TextSpan(
+                        text:
+                            "\u20b9${(item.price * item.quantity) - (item.quantity * item.discount)}\t\t",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                        children: [
+                          TextSpan(
+                              text: "\u20b9${item.price * item.quantity}",
+                              style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 16,
+                                  decoration: TextDecoration.lineThrough,
+                                  fontWeight: FontWeight.bold)),
+                          TextSpan(
+                              text:
+                                  "\t\tYou Save \u20b9${item.discount * item.quantity}",
+                              style: TextStyle(
+                                  color: Colors.green,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold)),
+                        ]),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  item.inCart
+                      ? Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            SizedBox(
+                              height: 30,
+                              width: 30,
+                              child: FlatButton(
+                                child: Icon(Icons.remove, color: Colors.white),
+                                onPressed: () {
+                                  if (item.quantity != 1) {
+                                    setState(() {
+                                      item.quantity--;
+                                    });
+                                  } else {
+                                    setState(() {
+                                      item.inCart = false;
+                                    });
+                                  }
+                                },
+                                color: Myapp.primaryColor,
+                                padding: EdgeInsets.zero,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50)),
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              item.quantity.toString(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                            SizedBox(width: 10),
+                            SizedBox(
+                              height: 30,
+                              width: 30,
+                              child: FlatButton(
+                                child: Icon(Icons.add, color: Colors.white),
+                                onPressed: item.quantity <= item.maxQuantity
+                                    ? () {
+                                        if (item.quantity != item.maxQuantity) {
+                                          setState(() {
+                                            item.quantity++;
+                                          });
+                                        }
+                                      }
+                                    : null,
+                                color: item.quantity >= item.maxQuantity
+                                    ? Myapp.primaryColor.withOpacity(0.7)
+                                    : Myapp.primaryColor,
+                                disabledColor:
+                                    Myapp.primaryColor.withOpacity(0.7),
+                                padding: EdgeInsets.zero,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50)),
+                              ),
+                            ),
+                          ],
+                        )
+                      : Align(
+                          alignment: Alignment.centerRight,
+                          child: SizedBox(
+                            height: 32,
+                            width: 95,
+                            child: FlatButton(
+                              child: Text("ADD",
+                                  style: TextStyle(color: Colors.white)),
+                              onPressed: () {
+                                setState(() {
+                                  item.inCart = true;
+                                });
+                                scaffoldKey.currentState.showSnackBar(SnackBar(
+                                    content:
+                                        Text("Added to cart successfully")));
+                              },
+                              color: Myapp.primaryColor,
+                            ),
+                          ),
+                        )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
@@ -265,4 +484,21 @@ class Category {
   bool isSelected;
 
   Category({this.title, this.image, this.isSelected: false});
+}
+
+class Item {
+  final String image, name, category;
+  final double price, maxQuantity, discount;
+  int quantity;
+  bool inCart;
+
+  Item(
+      {this.image,
+      this.name,
+      this.category,
+      this.price,
+      this.maxQuantity,
+      this.discount,
+      this.inCart: false,
+      this.quantity});
 }
